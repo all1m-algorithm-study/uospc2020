143d4031f6bd03be948e3576926a75b719c26502
yonsweng <yonsweng@gmail.com>
2020-11-21 10:45:40 +0900

    Add covid_20 sources

---
+++ problems/covid_20/source/covid_20_gen.cpp
@@ -0,0 +1,60 @@
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <vector>
+
+#include "testlib.h"
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+    registerGen(argc, argv, 1);
+
+    int n = atoi(argv[1]);
+    int m = atoi(argv[2]);
+    int l = atoi(argv[3]);
+    int c_min = atoi(argv[4]);
+    int c_max = atoi(argv[5]);
+
+    assert(1 <= n && n <= m);
+    assert(1 <= m && m <= 1000000);
+    assert(0 <= l && l <= 1000000);
+    assert(0 <= c_min && c_min <= c_max && c_max <= 1000000000);
+
+    vector<int> num_list(m + 1);
+    for (int i = 1; i <= m; i++)
+        num_list[i] = i;
+
+    cout << n << ' ' << m << ' ' << l << '\n';
+
+    // n명의 확진자 출력
+    for (int i = 1; i <= n - 1; i++) {
+        int rnd_num = rnd.next(i, m);
+        swap(num_list[i], num_list[rnd_num]);
+        cout << num_list[i] << ' ';
+    }
+    cout << num_list[n] << '\n';
+
+    vector<tuple<int, int, int>> edges;
+    for (int i = 0; i < l; i++) {
+        int a = rnd.next(1, m - 1);
+        int b = rnd.next(a + 1, m);
+        int c = rnd.next(c_min, c_max);
+        if (rnd.next(0, 1) == 1)
+            swap(a, b);
+        edges.emplace_back(a, b, c);
+    }
+    sort(edges.begin(), edges.end());
+    for (int i = 1; i < l; i++) {
+        assert(edges[i - 1] != edges[i]);
+    }
+
+    ::shuffle(edges.begin(), edges.end());
+
+    // edges 출력
+    for (auto edge : edges) {
+        cout << get<0>(edge) << ' ' << get<1>(edge) << ' ' << get<2>(edge) << '\n';
+    }
+
+    return 0;
+}
