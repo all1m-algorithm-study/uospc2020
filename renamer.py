7bbaeb4014f958194af24fc51ed12aff10d55092
yonsweng <yonsweng@gmail.com>
2020-11-21 12:52:38 +0900

    Add covid_20, best_strategy, x_mas test cases

---
+++ renamer.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+
+path_from = "problems/x_mas"
+
+for (path, dir, files) in os.walk(path_from):
+    if path[-5:] != "tests": continue
+    os.mkdir(path + '/testcases')
+    for filename in files:
+        src = path + '/' + filename
+        dst = path + '/testcases/' + filename
+        if filename[-2:] == '.a':
+            dst = dst[:-2] + '.out'
+        else:
+            dst = dst + '.in'
+        shutil.copy(src, dst)
